36922477d0ea1078fbc77bf3d912f556f7b3eabe
jardapernik <jaroslav.pernica9@gmail.com>
2020-07-16 13:01:05 +0200

    fix umd

--- assign-deep.js
+++ assign-deep.js
@@ -4,54 +4,53 @@
  * same as Object.assign, except deep merging nested objects
  * 
  * (c) JP 2020
+ * 
+ * Example:
+ * 
+ *   assignDeep( target, source1, source2, ..., sourceN );
+ * 
  */
 ( function( global, factory ) {
 	"use strict";
 	
 	if ( typeof module === "object" && typeof module.exports === "object" ) {
-		module.exports = factory( global );
+		module.exports = factory( global, true );
 	} else {
-		factory( global, true );
+		factory( global );
 	}
 } )( typeof window !== "undefined" ? window : this, function( window, global ) {
 	"use strict";
 
-	var assignDeep = ( function() {
-
-		function copyValue( key ) {
-			if ( !this.from.hasOwnProperty( key ) )
-				return;
-			
-			if ( typeof this.from[ key ] === "object" )
-				return assignDeep( this.to[ key ], this.from[ key ] );
-			
-			this.to[ key ] = this.from[ key ];
-		}
+	function copyValue( key ) {
+		if ( !this.from.hasOwnProperty( key ) )
+			return;
 		
-		function mergeTo( source ) {
-			if ( !source )
-				return;
-			
-			var keys = Object.keys( source );
-			
-			keys.forEach( copyValue, { from: source, to: this } );
-		}
+		if ( typeof this.from[ key ] === "object" )
+			return assignDeep( this.to[ key ], this.from[ key ] );
 		
-		function assignDeep( target ) {
-			if ( !target )
-				throw new TypeError(
-					"Cannot convert undefined or null to object" );
-			
-			var sources = Array.prototype.slice.call( arguments, 1 );
-			
-			sources.forEach( mergeTo, target );
-			
-			return target;
-		};
+		this.to[ key ] = this.from[ key ];
+	}
+	
+	function mergeTo( source ) {
+		if ( !source )
+			return;
+		
+		var keys = Object.keys( source );
+		
+		keys.forEach( copyValue, { from: source, to: this } );
+	}
 
-		return assignDeep;
-	})();
+	function assignDeep( target, source1, source2, sourceN ) {
+		if ( !target )
+			return "Cannot convert undefined or null to object";
+		
+		var sources = Array.prototype.slice.call( arguments, 1 );
+		
+		sources.forEach( mergeTo, target );
 
+		return false;
+	}
+	
 	if ( !global )
 		window.assignDeep = assignDeep;
 
